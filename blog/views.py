c2b76aba408b696a479f8337d63cf64bf18fcb18
jake-bestland <beja0405@gmail.com>
2024-09-19 11:58:15 -0500

    creating seperate blog repo

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Post
+
+
+# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'blog/index.html', context)
+
+def detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {'post': post}
+    return render(request, 'blog/detail.html', context)
